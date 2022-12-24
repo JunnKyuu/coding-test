fadf9f30ebb05ec2e2b76b416f2b46290e0ebc69
JunnKyuu <junkyu.dev@gmail.com>
2022-12-24 13:32:47 +0900

    오류 수정

---
+++ programmers/c++/string.cpp
@@ -0,0 +1,31 @@
+/*
+문자열안에 문자열
+
+문자열 str1, str2가 매개변수로 주어집니다. 
+str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요.
+*/
+
+
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int solution(string str1, string str2) {
+  int answer = 0;
+
+  return answer;
+}
+
+
+int main() {
+  string str1, str2;
+
+  cin >> str1;
+  cin >> str2;
+  
+  cout << str1 << endl;
+  cout << str2 << endl;
+}
