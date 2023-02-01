f6293ec9c5f1ed6a9886bce04e2d85a33029dabe
Jun <junkyu.dev@gmail.com>
2023-02-01 14:34:14 +0900

    Update OX퀴즈.cc

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -1,21 +1,30 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int main() {
-	int T;
-	string s;
-
-	cin >> T;
-	for (int j = 0; j < T; j++) {
-		cin >> s;
-		int sum, cnt;
-		sum = cnt = 0;
-		for (int i = 0; i < s.length(); i++) {
-			if (s[i] == 'O') cnt++;
-			else cnt = 0;
-			sum += cnt;
-		}
-		cout << sum<<'\n';
-	}
+    int n = 0;
+    string s;
+    
+    cin >> n;
+    
+    for(int i = 0; i<n; i++) {
+        cin >> s;
+        
+        int sum, count;
+        sum = count = 0;
+        
+        for(int j = 0; j<s.length(); j++) {
+            if(s[j] == 'O') {
+                count ++;
+            } else {
+                count = 0;
+            }
+            
+            sum += count;
+        }
+        
+        cout << sum << endl;
+    }
 }
