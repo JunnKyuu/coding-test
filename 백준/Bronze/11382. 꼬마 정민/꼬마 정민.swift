c422ba97b412fa23fae3cf1d645d6cfc14fbfb9a
Jun <JunnKyuu22@gmail.com>
2023-02-21 21:55:50 +0900

    [Bronze V] Title: 꼬마 정민, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let input = readLine()!
+
+let inputArr = input.components(separatedBy: " ")
+let a = Int(inputArr[0])!
+let b = Int(inputArr[1])!
+let c = Int(inputArr[2])!
+
+print(a + b + c)
