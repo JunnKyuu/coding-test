b39b808448f3f66611f0a52fed059808cc7cd28c
Jun <JunnKyuu22@gmail.com>
2024-09-09 15:28:51 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 240 ms, Memory: 76200 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.swift
@@ -0,0 +1,12 @@
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let n = input[0], m = input[1]
+var array = [0] + readLine()!.split(separator: " ").map { Int($0)! }
+for i in 1...n {
+    array[i] += array[i - 1]
+}
+
+for _ in 0..<m {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let i = input[0], j = input[1]
+    print(array[j] - array[i - 1])
+}
