faa3be141c48f3147fa02e525c0561105b6ba66a
Jun <JunnKyuu22@gmail.com>
2024-11-15 00:30:06 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 36 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.swift
@@ -0,0 +1,16 @@
+while let s = readLine(), s != "." {
+    var stack: [Character] = []
+    for s in s {
+        if !["[", "]", "(", ")"].contains(s) {
+            continue
+        }
+        if s == "]" && stack.last == "[" {
+            stack.removeLast()
+        } else if s == ")" && stack.last == "(" {
+            stack.removeLast()
+        } else {
+            stack.append(s)
+        }
+    }
+    print(stack.isEmpty ? "yes" : "no")
+}
