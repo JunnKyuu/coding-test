757a9baa4f7035936926bfdebe56d06838daf198
Jun <JunnKyuu22@gmail.com>
2025-10-22 13:34:49 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = ''
+    vowers = "aeiou"
+    
+    for i in my_string:
+        if i != 'a' and i != 'e' and i != 'i' and i != 'o' and i != 'u':
+            answer += i
+    
+    return answer
