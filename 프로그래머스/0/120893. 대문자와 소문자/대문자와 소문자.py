316c16ea25d96d01a2cf4442d45ac51c0c216001
Jun <JunnKyuu22@gmail.com>
2025-10-25 01:26:19 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.21 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = ''
+    
+    for i in my_string:
+        if ord(i) > 96 and ord(i) < 123:
+            answer += i.upper()
+        else:
+            answer += i.lower()
+            
+    return answer
